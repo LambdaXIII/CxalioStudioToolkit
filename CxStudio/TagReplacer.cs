7e4624ef396bc5605f2062e881ee8aba39ffc583
xiii_1991 <xiii_1991@163.com>
2025-02-14 16:39:12 +0800

    add function

--- CxStudio/TagReplacer.cs
+++ CxStudio/TagReplacer.cs
@@ -8,33 +8,6 @@ namespace CxStudio
         string Replace(string? param);
     }
 
-
-    public class TagReplacer
-    {
-        private readonly Dictionary<string, ITagStringProvider> _tagProviders;
-
-        public TagReplacer()
-        {
-            _tagProviders = new Dictionary<string, ITagStringProvider>();
-        }
-
-        public TagReplacer InstallProvider(string tag, ITagStringProvider provider)
-        {
-            _tagProviders.Add(tag, provider);
-            return this;
-        }
-
-        public string ReplaceTags(string input)
-        {
-            foreach (var tagProvider in _tagProviders)
-            {
-                string pattern = $@"\$\{{{tagProvider.Key}(:(.*?))?\}}";
-                input = Regex.Replace(input, pattern, match => tagProvider.Value.Replace(match.Groups[2].Success ? match.Groups[2].Value : null));
-            }
-            return input;
-        }
-    }
-
     public class FileInfoProvider : ITagStringProvider
     {
         private string _source;
@@ -59,5 +32,48 @@ namespace CxStudio
             return result ?? _source;
         }
     }
+
+    class SimpleReplacementProvider(string replacement) : ITagStringProvider
+    {
+        public string Replace(string? param)
+        {
+            return replacement;
+        }
+    }
+
+
+        public class TagReplacer
+    {
+        private readonly Dictionary<string, ITagStringProvider> _tagProviders;
+
+        public TagReplacer()
+        {
+            _tagProviders = new Dictionary<string, ITagStringProvider>();
+        }
+
+        public TagReplacer InstallProvider(string tag, ITagStringProvider provider)
+        {
+            _tagProviders.Add(tag, provider);
+            return this;
+        }
+
+        public TagReplacer InstallProvider(string tag, string replacement)
+        {
+            _tagProviders.Add(tag,new SimpleReplacementProvider(replacement));
+            return this;
+        }
+
+        public string ReplaceTags(string input)
+        {
+            foreach (var tagProvider in _tagProviders)
+            {
+                string pattern = $@"\$\{{{tagProvider.Key}(:(.*?))?\}}";
+                input = Regex.Replace(input, pattern, match => tagProvider.Value.Replace(match.Groups[2].Success ? match.Groups[2].Value : null));
+            }
+            return input;
+        }
+    }
+
+
 
 }//namespace
