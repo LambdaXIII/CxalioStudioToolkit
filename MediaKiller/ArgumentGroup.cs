4843fa6a5eb9089d7aa6e3dc8e82ff1054c5df96
xiii_1991 <xiii_1991@163.com>
2025-02-17 20:44:56 +0800

    ArgumentGroup

---
+++ MediaKiller/ArgumentGroup.cs
@@ -0,0 +1,66 @@
+﻿namespace MediaKiller;
+
+class ArgumentGroup
+{
+    public readonly Dictionary<string, string?> Options = new();
+    public string? FileName = null;
+
+    public static string CheckKey(string key)
+    {
+        key = key.Trim();
+        if (key[0] != '-')
+        {
+            key = '-' + key;
+        }
+        return key;
+    }
+
+    public ArgumentGroup AddArgument(string name, string? value = null)
+    {
+        string key = CheckKey(name);
+        Options[key] = value;
+        return this;
+    }
+
+    public ArgumentGroup AddArguments(string arguments)
+    {
+        string? last_key = null;
+        foreach (string arg in arguments.Split(' '))
+        {
+            if (arg[0] == '-')
+            {
+                last_key = arg;
+                AddArgument(arg);
+            }
+            else if (last_key is not null)
+            {
+                AddArgument(last_key, arg);
+                last_key = null;
+            }
+        }
+        return this;
+    }
+
+    public ArgumentGroup RemoveArgument(string name)
+    {
+        string key = CheckKey(name);
+        Options.Remove(key);
+        return this;
+    }
+
+    public IEnumerable<string> Arguments()
+    {
+        foreach (var (key, value) in Options)
+        {
+            if (value is null)
+            {
+                yield return key;
+            }
+            else
+            {
+                yield return key;
+                yield return value;
+            }
+        }
+    }
+}
--- MediaKiller/Mission.cs
+++ MediaKiller/Mission.cs
@@ -2,44 +2,12 @@
 
 sealed class Mission
 {
-    public struct OptionPackage
-    {
-        public string FileName;
-        public Dictionary<string, string?> Options;
-
-        public static string CheckKey(string key)
-        {
-            key = key.Trim();
-            if (key[0] != '-')
-            {
-                key = '-' + key;
-            }
-            return key;
-        }
-
-        public readonly IEnumerable<string> Arguments()
-        {
-            foreach (var (key, value) in Options)
-            {
-                if (value is null)
-                {
-                    yield return CheckKey(key);
-                }
-                else
-                {
-                    yield return CheckKey(key);
-                    yield return value;
-                }
-            }
-        }
-    }
-
     public string FFmpegPath = string.Empty;
 
-    public OptionPackage GlobalOptions = new();
+    public ArgumentGroup GlobalOptions = new();
 
-    public List<OptionPackage> Inputs = [];
-    public List<OptionPackage> Outputs = [];
+    public List<ArgumentGroup> Inputs = [];
+    public List<ArgumentGroup> Outputs = [];
 
     public List<string> CommandElements()
     {
@@ -48,8 +16,8 @@ sealed class Mission
             FFmpegPath,
             .. GlobalOptions.Arguments(),
         ];
-        Inputs.ForEach((input) => { cmd.AddRange(input.Arguments()); cmd.Add("-i"); cmd.Add(input.FileName); });
-        Outputs.ForEach(Outputs => { cmd.AddRange(Outputs.Arguments()); cmd.Add(Outputs.FileName); });
+        Inputs.ForEach((input) => { cmd.AddRange(input.Arguments()); cmd.Add("-i"); cmd.Add(input.FileName ?? ""); });
+        Outputs.ForEach(Outputs => { cmd.AddRange(Outputs.Arguments()); cmd.Add(Outputs.FileName ?? ""); });
         return cmd;
     }
 
