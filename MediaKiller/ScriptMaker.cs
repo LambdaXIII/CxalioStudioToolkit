ac054cf7950ec027120c6c600c936155bc98ce9c
xiii_1991 <xiii_1991@163.com>
2025-02-28 22:13:38 +0800

    Script maker

---
+++ MediaKiller/ScriptMaker.cs
@@ -0,0 +1,40 @@
+﻿using CxStudio;
+
+namespace MediaKiller;
+
+class ScriptMaker
+{
+    private readonly TextWriter Writer;
+    private readonly HashSet<string> Folders = [];
+
+    public ScriptMaker(TextWriter? writer)
+    {
+        Writer = writer ?? throw new ArgumentNullException(nameof(writer));
+    }
+
+    ~ScriptMaker()
+    {
+        Writer.Flush();
+        Writer.Close();
+    }
+
+    public void WriteMission(Mission mission)
+    {
+        foreach (ArgumentGroup oGroup in mission.Outputs)
+        {
+            string? folder = Path.GetDirectoryName(
+                Path.GetFullPath(oGroup.FileName));
+
+            if (folder is null || Folders.Contains(folder))
+            {
+                continue;
+            }
+
+            Folders.Add(folder);
+            Writer.WriteLine($"mkdir -p {TextUtils.QuoteSpacedString(folder)}");
+        }
+
+        Writer.WriteLine(mission.GetFullCommand());
+    }
+
+}
