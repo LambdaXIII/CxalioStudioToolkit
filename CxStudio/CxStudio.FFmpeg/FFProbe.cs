03e36d97f164d1d7018481db0ea4a6e30ab1d3b4
xiii_1991 <xiii_1991@163.com>
2025-03-27 13:47:49 +0800

    improvements

--- CxStudio/CxStudio.FFmpeg/FFProbe.cs
+++ CxStudio/CxStudio.FFmpeg/FFProbe.cs
@@ -15,49 +15,42 @@ public class FFprobe
 
     public MediaFormatInfo? GetFormatInfo(string source)
     {
-        try
+        string sourceFullPath = Path.GetFullPath(source);
+        ProcessStartInfo info = new()
         {
-            string sourceFullPath = Path.GetFullPath(source);
-            ProcessStartInfo info = new()
-            {
-                FileName = FFprobeBin,
-                Arguments = $"-v quiet -print_format json -show_format {TextUtils.QuoteSpacedString(sourceFullPath)}",
-                RedirectStandardError = true,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                StandardOutputEncoding = Encoding.UTF8,
-                StandardErrorEncoding = Encoding.UTF8
-            };
+            FileName = FFprobeBin,
+            Arguments = $"-v quiet -print_format json -show_format {TextUtils.QuoteSpacedString(sourceFullPath)}",
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            StandardOutputEncoding = Encoding.UTF8,
+            StandardErrorEncoding = Encoding.UTF8
+        };
 
-            Process process = new()
-            {
-                StartInfo = info,
-                EnableRaisingEvents = true
-            };
-            process.Start();
-            process.WaitForExit();
-
-            JsonNode formatNode = JsonNode.Parse(process.StandardOutput.ReadToEnd())!["format"]!;
-
-            var tags = formatNode["tags"]?.AsObject()?.ToDictionary(x => x.Key, x => x.Value!.GetValue<string>());
-
-            return new MediaFormatInfo
-            {
-                FullPath = sourceFullPath,
-                StreamCount = formatNode["nb_streams"]!.GetValue<uint>(),
-                FormatName = formatNode["format_name"]!.GetValue<string>()!,
-                FormatLongName = formatNode["format_long_name"]!.GetValue<string>()!,
-                StartTime = Time.FromSeconds(double.Parse(formatNode["start_time"]!.GetValue<string>()!)),
-                Duration = Time.FromSeconds(double.Parse(formatNode["duration"]!.GetValue<string>()!)),
-                Size = FileSize.FromBytes(ulong.Parse(formatNode["size"]!.GetValue<string>()!)),
-                Bitrate = FileSize.FromString(formatNode["bit_rate"]!.GetValue<string>()!),
-                Tags = tags ?? []
-            };
-        }
-        catch (Exception)
+        Process process = new()
         {
-            return null;
-        }
+            StartInfo = info,
+            EnableRaisingEvents = true
+        };
+        process.Start();
+        process.WaitForExit();
+
+        JsonNode formatNode = JsonNode.Parse(process.StandardOutput.ReadToEnd())!["format"]!;
+
+        var tags = formatNode["tags"]?.AsObject()?.ToDictionary(x => x.Key, x => x.Value!.GetValue<string>());
+
+        return new MediaFormatInfo
+        {
+            FullPath = sourceFullPath,
+            StreamCount = formatNode["nb_streams"]!.GetValue<uint>(),
+            FormatName = formatNode["format_name"]!.GetValue<string>()!,
+            FormatLongName = formatNode["format_long_name"]!.GetValue<string>()!,
+            StartTime = Time.FromSeconds(double.Parse(formatNode["start_time"]!.GetValue<string>()!)),
+            Duration = Time.FromSeconds(double.Parse(formatNode["duration"]!.GetValue<string>()!)),
+            Size = FileSize.FromBytes(ulong.Parse(formatNode["size"]!.GetValue<string>()!)),
+            Bitrate = FileSize.FromString(formatNode["bit_rate"]!.GetValue<string>()!),
+            Tags = tags ?? []
+        };
     }
 }
